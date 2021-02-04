44cd06f888b41a1059cd7c8f4c99561cd8bbbcb1
beendra <alexandra.ricardel@gmail.com>
2021-02-04 15:37:13 -0600

    test

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+  @@all = []
+
+  attr_reader :name, :species
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def self.count
+    self.all.count
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
+  
 end
