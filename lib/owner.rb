4600391952be4d35c4fc81b01e3b6dcfe180fea9
beendra <alexandra.ricardel@gmail.com>
2021-02-04 16:16:12 -0600

    part three

--- lib/owner.rb
+++ lib/owner.rb
@@ -24,5 +24,25 @@ class Owner
   def self.reset_all
     self.all.clear
   end
-  
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dog
+    mood = "happy"
+  end
+
 end
