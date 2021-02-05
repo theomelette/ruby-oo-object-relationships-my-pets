5e61114ca0aedf84a61ff4ee2d578093e92d9f13
Linda Hendricks <linda.timoh@gmail>
2021-02-05 09:30:32 -0500

    commit tree 94628e3dccf75ecd7bd9997c9336914ca8556b5f

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,49 @@
-class Owner
-  # code goes here
-end
+
+
+  class Owner
+
+    @@all = []
+    attr_reader :name, :species
+
+    def initialize(name)
+      @name = name
+      @species = "human"
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def say_species
+      "I am a human."
+    end
+
+    def self.count
+      self.all.count
+    end
+
+    def self.reset_all
+      self.all.clear
+    end
+
+    def cats
+      Cat.all.select {|cat| cat.owner == self}
+    end
+
+    def dogs
+      Dog.all.select {|dog| dog.owner == self}
+    end
+
+    def buy_cat(name)
+      Cat.new(name, self)
+    end
+
+    def buy_dog(name)
+      Dog.new(name, self)
+    end
+
+    def walk_dog
+      @mood = "happy"
+    end
+  end
